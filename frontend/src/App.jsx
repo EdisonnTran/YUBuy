03f3feb463140b46def6701e19cda187bad93358
OrionPi3 <reg_univ@outlook.com>
2026-06-24 23:15:52 -0400

    add seller profile page with active and sold listings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ListingDetail from './pages/ListingDetails'
+import SellerProfile from './pages/SellerProfile'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
          <Route path="/login" element={<Login />} />
          <Route path="/register" element={<Register />} />
          <Route path="/listings/:id" element={<ListingDetail />} />
+         <Route path="/profile" element={<SellerProfile />} />
 
       </Routes>
     </BrowserRouter>
