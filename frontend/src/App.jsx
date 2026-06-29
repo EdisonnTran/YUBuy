1d79d1b2061122030c0d3be54d7847fa2d73d861
Ahmed Daniyal <daniyalahmedala@gmail.com>
2026-06-29 19:19:17 -0400

    Add listings page UI with filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Listings from './pages/Listings'
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
          <Route path="/" element={<Register />} />
          <Route path="/login" element={<Login />} />
          <Route path="/register" element={<Register />} />
-
+         <Route path="/listings" element={<Listings />} />
       </Routes>
     </BrowserRouter>
   )
