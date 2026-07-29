b730aa3134d4e9f00dd0861f7912903a33b87c37
EdisonnTran <tran.edison52@gmail.com>
2026-07-29 16:33:30 -0400

    Trying port 587

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -3,6 +3,8 @@ import nodemailer from 'nodemailer'
 //Sets up email transporter using Gmail and YUBuy email creds in .env 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
